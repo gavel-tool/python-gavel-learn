4685b951a3c961b3d52d96ddbb1a2d8f375d6b42
MGlauer <martinglauer89@gmail.com>
2021-01-04 12:21:45 +0100

    Register entry points

--- setup.py
+++ setup.py
@@ -81,4 +81,10 @@ setup(
         #   'rst': ['docutils>=0.11'],
         #   ':python_version=="2.6"': ['argparse'],
     },
+    entry_points={
+        "cli": [
+            "gavel_learn = gavel_learn.cli:learn",
+        ],
+        "gavel.plugins": "learn = gavel_learn",
+    },
 )
