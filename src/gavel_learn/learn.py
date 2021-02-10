b8a4b52036c9d2a4740641e19c5f52069f9bb6d6
MGlauer <martinglauer89@gmail.com>
2021-02-10 11:29:39 +0100

    Add residual connection with attention

--- src/gavel_learn/learn.py
+++ src/gavel_learn/learn.py
@@ -7,21 +7,16 @@ import numpy as np
 from matplotlib import pyplot as plt
 from itertools import chain
 
-if torch.cuda.is_available():
-    DEVICE = torch.device("cuda:0")
-else:
-    DEVICE = torch.device("cpu:0")
-
 class FormulaNet(torch.nn.Module, Compiler):
 
-    def __init__(self, device=DEVICE):
+    def __init__(self):
         super().__init__()
-        self.device = device
         self._leaf_factor = 3
         self._leaf_offset = 5
         self.length = 50
         self.argument_limit = 5
         self.max_embeddings = 100
+        self.attention = Attention(self.length)
         self.output_size = self._leaf_factor*self.length + self._leaf_offset
         self.unary_formula = torch.nn.Linear(self.length, self.length)
         self.final = torch.nn.Linear(self.length, self.output_size)
@@ -32,15 +27,22 @@ class FormulaNet(torch.nn.Module, Compiler):
         self.functor_formula = torch.nn.Linear((1 + self.argument_limit) * self.length, self.length)
         self.leaf_net = torch.nn.Linear(self._leaf_factor * self.length + self._leaf_offset, self.length)
 
-        self.embeddings = dict(
-            constants=torch.nn.Embedding(self.max_embeddings, self.length).to(self.device),
-            functors = torch.nn.Embedding(self.max_embeddings, self.length).to(self.device),
-            predicates = torch.nn.Embedding(self.max_embeddings, self.length).to(self.device),
-            binary_operators = torch.nn.Embedding(self.max_embeddings, self.length).to(self.device),
-            variables = torch.nn.Embedding(self.max_embeddings, self.length).to(self.device))
+        self._ce = torch.nn.Embedding(self.max_embeddings, self.length)
+        self._fe = torch.nn.Embedding(self.max_embeddings, self.length)
+        self._pe = torch.nn.Embedding(self.max_embeddings, self.length)
+        self._oe = torch.nn.Embedding(self.max_embeddings, self.length)
+        self._ve = torch.nn.Embedding(self.max_embeddings, self.length)
 
-        self._null = torch.autograd.Variable(torch.randn(self.length)).to(self.device)
-        self._masked = torch.zeros(self._leaf_factor * self.length + self._leaf_offset).to(self.device)
+        self.embeddings = dict(
+            constants=self._ce,
+            functors = self._fe,
+            predicates = self._pe,
+            binary_operators = self._oe,
+            variables = self._ve
+        )
+
+        self._null = torch.autograd.Variable(torch.randn(self.length))
+        self._masked = torch.zeros(self._leaf_factor * self.length + self._leaf_offset)
 
     """def _generate_encodings(self, index):
         identifiers = torch.stack(
@@ -53,7 +55,7 @@ class FormulaNet(torch.nn.Module, Compiler):
 
     def visit_encoded_element(self, elem:EncodedElement):
         if elem.value < self.max_embeddings:
-            return self.embeddings[elem.kind](torch.tensor([elem.value]).to(self.device)).squeeze(0)
+            return self.embeddings[elem.kind](torch.tensor(elem.value))
         else:
             return self._null
 
@@ -73,7 +75,8 @@ class FormulaNet(torch.nn.Module, Compiler):
         return self.visit(anno.formula)
 
     def visit_binary_formula(self, formula: fol.BinaryFormula):
-        return torch.relu(self.binary_formula(torch.cat([self.visit(formula.left),self.visit(formula.operator), self.visit(formula.right)], dim=-1)))
+        ops = [self.visit(formula.left),self.visit(formula.operator), self.visit(formula.right)]
+        return torch.relu(self.binary_formula(torch.cat(ops, dim=-1))) + self.attention(torch.stack(ops))
 
     def visit_functor_expression(self, expression: fol.FunctorExpression, **kwargs):
         arguments = [self.visit(expression.functor)]
@@ -81,7 +84,7 @@ class FormulaNet(torch.nn.Module, Compiler):
         # Fill missing arguments with zeros
         for _ in range(len(arguments)+1, self.argument_limit+2):
             arguments.append(self._null)
-        return torch.relu(self.functor_formula(torch.cat(arguments, dim=-1)))
+        return torch.relu(self.functor_formula(torch.cat(arguments, dim=-1))) + self.attention(torch.stack(arguments))
 
     def visit_predicate_expression(self, expression: fol.PredicateExpression, **kwargs):
         arguments = [self.visit(expression.predicate)]
@@ -89,42 +92,41 @@ class FormulaNet(torch.nn.Module, Compiler):
         # Fill missing arguments with zeros
         for _ in range(len(arguments)+1, self.argument_limit+2):
             arguments.append(self._null)
-        return torch.relu(self.predicate_formula(torch.cat(arguments, dim=-1)))
+        return torch.relu(self.predicate_formula(torch.cat(arguments, dim=-1))) + self.attention(torch.stack(arguments))
 
     def visit_masked(self, masked: MaskedElement):
         return self._masked
 
 
-class Residual(torch.nn.Module):
-    def __init__(self, sublayer: torch.nn.Module, dimension: int, dropout: float = 0.1):
+class Attention(torch.nn.Module):
+    def __init__(self, n):
         super().__init__()
-        self.sublayer = sublayer
-        self.norm = torch.nn.LayerNorm(dimension)
-        self.dropout = torch.nn.Dropout(dropout)
+        self.weights = torch.nn.Linear(n,1)
 
-    def forward(self, *tensors: torch.Tensor) -> torch.Tensor:
-        # Assume that the "value" tensor is given last, so we can compute the
-        # residual.  This matches the signature of 'MultiHeadAttention'.
-        return self.norm(tensors[-1] + self.dropout(self.sublayer(*tensors)))
+    def forward(self, input):
+        filter = torch.softmax(self.weights(input), dim=0).t()
+        return torch.sum(torch.matmul(filter, input), dim=0)
+
 
 class PremiseSelector(torch.nn.Module):
 
     def __init__(self):
         super().__init__()
         self.formula_net = FormulaNet()
-        self.formula_net.to(self.formula_net.device)
         self.encoder = torch.nn.GRU(self.formula_net.output_size, self.formula_net.output_size)
         self.conjecture_squash = torch.nn.GRU(self.formula_net.output_size, self.formula_net.output_size)
         self.scoring = torch.nn.Linear(self.formula_net.output_size*3, self.formula_net.output_size)
         self.softmax = torch.nn.Softmax(-1)
         self.decoder = torch.nn.GRU(self.formula_net.output_size, self.formula_net.output_size)
         self.final = torch.nn.Linear(self.formula_net.output_size, 1)
-        self.device = self.formula_net.device
 
-    def attention_loop(self, premise_stack, hidden, conj, thought):
+    def attention_loop(self, premise_stack, hidden, conj, thought=None):
         ratings = self.softmax(self.scoring(torch.cat((premise_stack, conj, hidden), dim=-1)))
         context = torch.sum(torch.mul(premise_stack, ratings), dim=0, keepdim=True)
-        thought = torch.cat((thought, context))
+        if thought is None:
+            thought = context.unsqueeze(0)
+        else:
+            thought = torch.cat((thought, context))
         o, hidden = self.decoder.forward(thought)
         return torch.sigmoid(self.final(o[-1])).squeeze(-1), hidden
 
@@ -134,13 +136,13 @@ class PremiseSelector(torch.nn.Module):
         num_prem = premise_stack.size()[0]
         hidden = premise_stack[-1]
         conj = torch.relu(self.conjecture_squash.forward((torch.stack([self.formula_net.forward(c) for c in conjectures]).unsqueeze(1)))[0].expand(num_prem,1,self.formula_net.output_size))
-        thought = torch.empty(1, 1, self.formula_net.output_size).to(self.device)
+        thought = None
         output = []
         for i in range(num_prem):
             hidden = hidden.expand(num_prem, 1, self.formula_net.output_size)
             o, hidden = self.attention_loop(premise_stack, hidden, conj, thought)
             output.append(o)
-        return torch.cat(output).to(self.device)
+        return torch.cat(output)
 
 
 class PremiseSelectorGRU(torch.nn.Module):
@@ -149,12 +151,10 @@ class PremiseSelectorGRU(torch.nn.Module):
         super().__init__()
 
         self.formula_net = FormulaNet()
-        self.formula_net.to(self.formula_net.device)
-        self.device = self.formula_net.device
         self.conjecture_squash = torch.nn.GRU(self.formula_net.output_size, self.formula_net.output_size)
         self.gru = torch.nn.GRU(self.formula_net.output_size*2, self.formula_net.output_size, bidirectional=True)
         self.final = torch.nn.Linear(self.formula_net.output_size * 2, 1)
-        self._nothing = torch.autograd.Variable(torch.rand((1,self.formula_net.output_size)).to(self.device))
+        self._nothing = torch.autograd.Variable(torch.rand((1,self.formula_net.output_size)))
 
     def forward(self, data):
         premise_stack = torch.nn.utils.rnn.pad_sequence([torch.stack([self.formula_net.forward(p) for p in batch[0]]) for batch in data])
@@ -163,7 +163,6 @@ class PremiseSelectorGRU(torch.nn.Module):
              data])
         num_prem = premise_stack.size()[0]
         batches = premise_stack.size()[1]
-        print(num_prem)
         conj = torch.relu(self.conjecture_squash.forward(conjecture_stack)[0][-1].expand(num_prem,batches,self.formula_net.output_size))
         o, h = self.gru(torch.cat((premise_stack, conj), dim=-1))
         return self.final(o).squeeze(-1)
@@ -171,11 +170,10 @@ class PremiseSelectorGRU(torch.nn.Module):
 
 def train_selection(gen):
     net = PremiseSelectorGRU()
-    net.to(net.device)
     optimizer = torch.optim.Adam(net.parameters())
     loss = torch.nn.MSELoss()
     learning_curve = []
-    for epoch in range(10):
+    for epoch in range(100):
         print("Epoch", epoch)
         i = 0
         batch_loss = 0
@@ -185,7 +183,7 @@ def train_selection(gen):
             print("Process batch", batchnumber)
             optimizer.zero_grad()
             predictions = net.forward(data)
-            ls = torch.nn.utils.rnn.pad_sequence([torch.tensor(l) for l in labels]).to(net.device)
+            ls = torch.nn.utils.rnn.pad_sequence([torch.tensor(l) for l in labels])
             l = loss(predictions, ls)
             l.backward()
             batch_loss += l.item()
@@ -203,7 +201,6 @@ def train_selection(gen):
 
 def train_masked(gen):
     net = FormulaNet()
-    net.to(net.device)
     mc = MaskCompiler()
     optimizer = torch.optim.Adam(net.parameters())
     loss = torch.nn.MSELoss()
